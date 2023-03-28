0bad1eb5c0b2a9956fe916ad994ba6ddc2406181
오재완 <dhwodhks147@gmail.com>
2023-03-28 15:41:20 +0900

    This

---
+++ DeepCopy/MainApp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DeepCopy
+{
+    class MyClass
+    {
+        public int field1;
+        public int field2;
+
+        public MyClass DeepCopy()
+        {
+            MyClass newCopy = new MyClass();
+            newCopy.field1 = this.field1;
+            newCopy.field2 = this.field2;
+
+            return newCopy;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shallow Copy");
+
+            {
+                MyClass source = new MyClass();
+                source.field1 = 10;
+                source.field2 = 20;
+
+                MyClass target = source;
+                target.field2 = 30;
+
+                Console.WriteLine($"{source.field1}, {source.field2}");
+                Console.WriteLine($"{target.field1}, {target.field2}");
+            }
+
+            Console.WriteLine("Deep Copy");
+            {
+                MyClass source = new MyClass();
+                source.field1 = 10;
+                source.field2 = 20;
+
+                MyClass target = source.DeepCopy();
+                target.field2 = 30;
+
+                Console.WriteLine($"{source.field1}, {source.field2}");
+                Console.WriteLine($"{target.field1}, {target.field2}");
+            }
+        }
+    }
+}
---
+++ This/MainApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace This
+{
+    class Employee
+    {
+        private string name;
+        private string position;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        
+        public void SetPosition(string position)
+        {
+            this.position = position;
+        }
+
+        public string GetPosition()
+        {
+            return position;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh");
+            pooh.SetPosition("Waiter");
+            Console.WriteLine($"{pooh.GetName()}, {pooh.GetPosition()}");
+
+            Employee tigger = new Employee();
+            tigger.SetName("Tigger");
+            tigger.SetPosition("Cleaner");
+            Console.WriteLine($"{tigger.GetName()}, {tigger.GetPosition()}");
+        }
+    }
+}
