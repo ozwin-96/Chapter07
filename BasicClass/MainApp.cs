863baad435bf00faa71be36af3c62decec67042d
오재완 <dhwodhks147@gmail.com>
2023-03-28 13:58:37 +0900

    초기 커밋.

---
+++ BasicClass/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string name;
+        public string color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{name} : 야옹");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.name = "키티";
+            kitty.color = "하얀색";
+            kitty.Meow();
+            Console.WriteLine($"{kitty.name} : {kitty.color}");
+
+            Cat nero = new Cat();
+            nero.name = "네로";
+            nero.color = "검은색";
+            nero.Meow();
+            Console.WriteLine($"{nero.name} : {nero.color}");
+        }
+    }
+}
---
+++ Constructor/MainApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Cat
+    {
+        public string name;
+        public string color;
+
+        public Cat()
+        {
+            name = "";
+            color = "";
+        }
+        public Cat(string _name, string _color)
+        {
+            name = _name;
+            color = _color;
+        }
+        ~Cat()
+        {
+            Console.WriteLine($"{name} : 잘가");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{name} : 야옹");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat("키티", "하얀색");
+            kitty.Meow();
+            Console.WriteLine($"{kitty.name} : {kitty.color}");
+
+            Cat nero = new Cat("네로", "검은색");
+            nero.Meow();
+            Console.WriteLine($"{nero.name} : {nero.color}");
+        }
+    }
+}
